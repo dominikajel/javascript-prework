25c307b0b7f417e3201d3974eb3485ca4364ff01
Dominika Jelonek <jelonekdominika@gmail.com>
2020-10-25 14:17:19 +0100

    Change script.js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var computerMove;
+
+var playerMove;
+
+var randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log("Wylosowana liczba to: " + randomNumber);
+
+if (randomNumber == "1") {
+  computerMove = "kamień";
+} else if (randomNumber == "2") {
+  computerMove = "papier";
+} else if (randomNumber == "3") {
+  computerMove = "nożyce";
+} else {
+  computerMove = "nieznany ruch";
+}
+
+printMessage("Mój ruch:" + computerMove);
